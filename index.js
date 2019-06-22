3ac728a0cc7fbb775cd0da5b4c9912d6d1cee933
Max Isom <code@maxisom.me>
2019-06-22 15:59:20 -0500

    Inital commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const Address = require('./lib/Address');
+const Customer = require('./lib/Customer');
+const Item = require('./lib/Item');
+const Store = require('./lib/Store');
+
+module.exports = {Address, Customer, Item, Store};
